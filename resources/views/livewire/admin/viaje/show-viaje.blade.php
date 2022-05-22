cdebc3ee65ee0e9140546df643d76694340ee8be
Nataliaqg <nataliaquirogag@gmail.com>
2022-05-21 20:22:50 -0400

    viaje primer commit

---
+++ resources/views/livewire/admin/viaje/show-viaje.blade.php
@@ -0,0 +1,11 @@
+<div><x-slot name="header">
+    <div class="flex items-center">
+        <h2 class="font-semibold text-xl text-gray-600 leading-tight">
+            Lista de Viajes
+        </h2>
+
+       
+    </div>
+    </x-slot>
+</div>
+
--- routes/admin.php
+++ routes/admin.php
@@ -10,13 +10,13 @@ use App\Http\Livewire\Admin\Lugar\CreateLugarturistico;
 use App\Http\Livewire\Admin\Lugar\EditLugarturistico;
 use App\Http\Livewire\Admin\Lugar\ShowLugarTuristico;
 use App\Http\Livewire\Admin\Usuario\UserComponent;
-
+use App\Http\Livewire\Admin\Viaje\ShowViaje;
 
 Route::get("",Home::class)->name('admin.index');
 //Route::get('',[HomeController::class,'index'])->name('admin.index');
 
-//Rutas CIUDAD
-Route::get('/ciudad', ShowCiudad::class)->name('ciudad.show');
+//Ruta ciudad
+Route::get('ciudad', ShowCiudad::class)->name('admin.ciudad.show');
 Route::get('ciudad/create',CreateCiudad::class)->name('admin.ciudad.create');
 Route::get('ciudad/{ciudad}/edit',EditCiudad::class)->name('admin.ciudad.edit');
 
@@ -27,3 +27,6 @@ Route::get('users',UserComponent::class)->name('admin.users.index');
 Route::get('lugarturisticos', ShowLugarTuristico::class)->name('admin.lugarturistico.show');
 Route::get('lugarturisticos/{lugarturistico}/edit', EditLugarturistico::class)->name('admin.lugarturisticos.edit');
 Route::get('lugarturisticos/create', CreateLugarturistico::class)->name('admin.lugarturisticos.create');
+
+//Ruta Viaje
+Route::get('viaje',ShowViaje::class)->name('admin.viaje.show');
