fc0fa6c30d66040ba3ccf288d2f10d51e7753f30
Nataliaqg <nataliaquirogag@gmail.com>
2022-05-24 01:30:15 -0400

    leves cambios

--- resources/views/livewire/admin/lugar/show-lugar-turistico.blade.php
+++ resources/views/livewire/admin/lugar/show-lugar-turistico.blade.php
@@ -8,8 +8,7 @@
                 class=" absolute top-15 right-5 p-sm inline-block px-6 py-2 border-2 border-green-500 text-green-500 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
                 agregar un nuevo lugar
             </a>
-
-
+        </div>
     </x-slot>
 
     <div class=" container first-line:py-12">
--- resources/views/livewire/admin/viaje/show-viaje.blade.php
+++ resources/views/livewire/admin/viaje/show-viaje.blade.php
@@ -83,7 +83,7 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="text-sm text-gray-900">
-                                        {{ $viaje->precio }}
+                                        {{ $viaje->precio }} BS
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
