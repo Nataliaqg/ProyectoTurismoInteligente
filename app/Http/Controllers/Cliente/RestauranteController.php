a6fa3b8e0f69712936938bdf0d1689646e0df0f4
Nataliaqg <nataliaquirogag@gmail.com>
2022-06-30 00:34:10 -0400

    vista ind rest

--- app/Http/Controllers/Cliente/RestauranteController.php
+++ app/Http/Controllers/Cliente/RestauranteController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Cliente;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurante;
 use Illuminate\Http\Request;
 
 class RestauranteController extends Controller
 {
-    //
+    public function show(Restaurante $restaurante){
+        return view('cliente.restaurante.show',compact('restaurante'));
+    }
 }
